69f3ddc39cff762ef2abc145986ddb001ab36fea
Dmitry <korkanovichm@yandex.ru>
2026-03-23 15:27:38 +0300

    [#1] feat: addded login page & login logic

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,8 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import Dashboard from "../pages/Dashboard/Dashboard";
-import TasksPage from "../pages/Tasks/TaskPage";
+import LoginPage from "../components/LoginPage";
 
 export const router = createBrowserRouter([
+    { path: "/login", element: <LoginPage /> },
     { path: "/", element: <Dashboard /> },
-    { path: "/tasks", element: <TasksPage /> },
+
 ]);
--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -4,11 +4,18 @@ export default function ThemeToggle() {
     const { theme, toggleTheme } = useTheme();
 
     return (
-        <button
-            onClick={toggleTheme}
-            className="px-4 py-2 rounded bg-gray-200 dark:bg-gray-700"
-        >
-            {theme === "dark" ? "☀️ Light Mode" : "🌙 Dark Mode"}
-        </button>
+        <div className="fixed top-4 right-4 z-50">
+            <button
+                onClick={toggleTheme}
+                className="
+                    px-4 py-2 rounded-lg shadow
+                    bg-white dark:bg-gray-800
+                    text-black dark:text-white
+                    hover:scale-105 transition
+                "
+            >
+                {theme === "dark" ? "☀️" : "🌙"}
+            </button>
+        </div>
     );
 }
